3b7b931508d476bc173dd046c17c7486af0b2ea5
Brian de Regt <brian.deregt@gmail.com>
2020-05-11 20:09:15 -0400

    typo

--- seed/seed.js
+++ seed/seed.js
@@ -1,6 +1,6 @@
 const db = require('../db/index.js');
 const photoUrl = require('./photoUrl.js');
-const schema = requre('./schema.sql');
+const schema = require('./schema.sql');
 
 const homeSeeder = (idx) => {
   for (let idx = 1; idx <= 100; idx += 1) {
